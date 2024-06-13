2a57790df7abb56473f9f64d6988154e41fad877
Abdullah M Osman <69367607+Abdellaosman@users.noreply.github.com>
2024-06-12 22:11:02 -0600

    Update Food_vision.py

--- scripts/Food_vision.py
+++ scripts/Food_vision.py
@@ -4,7 +4,7 @@ from prediction import predict_image
 from api_request import get_food_info
 
 # Predict the class of the image
-image_path = "C:/Users/Abdella/OneDrive/Desktop/summer2024classes/AI/finalpoject/sample_pics/03.jpg"
+image_path = "sample_pics/03.jpg"
 predicted_class = predict_image(image_path)
 
 # Get food information based on the predicted class
